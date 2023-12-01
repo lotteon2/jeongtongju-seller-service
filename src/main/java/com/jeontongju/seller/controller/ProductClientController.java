c7041cddbaf8b1a0f06be94a9fd4959677ca9e79
SOYOUNG CHOI <66711073+cso6005@users.noreply.github.com>
2023-12-01 12:04:35 +0900

    FEAT
    add getSellerInfoForCreateProduct feign api (product-service)
    
    SCRUM-146

--- src/main/java/com/jeontongju/seller/controller/ProductClientController.java
+++ src/main/java/com/jeontongju/seller/controller/ProductClientController.java
@@ -1,8 +1,28 @@
 package com.jeontongju.seller.controller;
 
+import com.jeontongju.seller.dto.temp.FeignFormat;
+import com.jeontongju.seller.dto.temp.SellerInfoDto;
+import com.jeontongju.seller.service.SellerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class ProductClientController {}
+public class ProductClientController {
+
+  private final SellerService sellerService;
+
+  @GetMapping("/sellers/{sellerId}/name-image")
+  FeignFormat<SellerInfoDto> getSellerInfoForCreateProduct(@PathVariable Long sellerId) {
+
+    System.out.println(sellerService.getSellerInfo(sellerId).getStoreName());
+
+    return FeignFormat.<SellerInfoDto>builder()
+        .code(HttpStatus.OK.value())
+        .data(sellerService.getSellerInfo(sellerId))
+        .build();
+  }
+}
